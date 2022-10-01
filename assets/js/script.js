947cd3bd432494bf6da06c7cff5f565cb28f0075
ahsanaaxioned <ahsana@axioned.com>
2022-10-01 22:54:18 +0530

    js 6th task done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,28 @@
 // js 1st task start here
-for(let i = 1 ; i<= 7 ; i+=2){
-    for( let k = i ; k<7 ; k++){
-        document.write("&nbsp;");
-    }
-    for(let j = 1 ; j<=i ; j++){
-        // document.write("*");
-        if(j % 2 == 0){
-            document.write("A");
-        }else{
-            document.write("*");
-        }
+// for(let i = 1 ; i<= 7 ; i+=2){
+//     for( let k = i ; k<7 ; k++){
+//         document.write("&nbsp;");
+//     }
+//     for(let j = 1 ; j<=i ; j++){
+//         // document.write("*");
+//         if(j % 2 == 0){
+//             document.write("A");
+//         }else{
+//             document.write("*");
+//         }
+//     }
+//     document.write("<br>");
+// }
+// js 1st task end here
+// js 6th task start here
+for(let i = 1; i <= 6 ; i++){
+    // document.write("*");
+    for(let j = 1 ; j<= i ; j+=1){
+         document.write("#");
     }
     document.write("<br>");
 }
-// js 1st task end here
-
+// js 6th task end here
 
 
 
