0fceb13dcea59d1605c8ff197fc2d85d4bc0fac5
ahsanaaxioned <ahsana@axioned.com>
2022-10-01 22:50:26 +0530

    js 1st task completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
-/* Author: 
-
-*/
-
+// js 1st task start here
+for(let i = 1 ; i<= 7 ; i+=2){
+    for( let k = i ; k<7 ; k++){
+        document.write("&nbsp;");
+    }
+    for(let j = 1 ; j<=i ; j++){
+        // document.write("*");
+        if(j % 2 == 0){
+            document.write("A");
+        }else{
+            document.write("*");
+        }
+    }
+    document.write("<br>");
+}
+// js 1st task end here
 
 
 
