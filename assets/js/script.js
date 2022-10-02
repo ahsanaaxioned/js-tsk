eec367f6729a1fdf0149d8b6e412a609d15fdacb
ahsanaaxioned <ahsana@axioned.com>
2022-10-02 13:31:09 +0530

    JS 2nd and 3rd task completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,16 +14,36 @@
 //     document.write("<br>");
 // }
 // js 1st task end here
-// js 6th task start here
-for(let i = 1; i <= 6 ; i++){
-    // document.write("*");
-    for(let j = 1 ; j<= i ; j+=1){
-         document.write("#");
-    }
-    document.write("<br>");
-}
-// js 6th task end here
-
+// // js 6th task start here
+// for(let i = 1; i <= 6 ; i++){
+//     // document.write("*");
+//     for(let j = 1 ; j<= i ; j++){
+//          document.write("#");
+//     }
+//     document.write("<br>");
+// }
+// // js 6th task end here
+// JS 2nd task start here
+// for(let row = 1 ; row<= 5 ;row++){
+//     for( let col = 1 ; col <= 9 ; col++){
+//         if( (row ==5) || (row+col==6) || (col-row==4)){
+//             document.write("*");
+//         }else{
+//             document.write("&nbsp;&nbsp");
+           
+//         }
+//     }
+//     document.write("<br>");
+// }
+// // JS 2nd task end here
+// JS 3rd task start here
+// for ( let i = 1 ; i<= 3 ; i++){
+//     for( let j = 1 ; j<= i ; j++){
+//         document.write(j**i + " ");
+//     }
+//     document.write("<br>");
+// }
+// JS 3rd task end here
 
 
 
