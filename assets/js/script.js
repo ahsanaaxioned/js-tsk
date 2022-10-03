5e86db851bf84c24e8e2f505eabcafdd7fe87fc0
ahsanaaxioned <ahsana@axioned.com>
2022-10-03 16:46:35 +0530

    git deploy done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,72 @@
-// // js 1st task start here
-// // for(let i = 1 ; i<= 7 ; i+=2){
-// //     for( let k = i ; k<7 ; k++){
-// //         document.write("&nbsp;");
-// //     }
-// //     for(let j = 1 ; j<=i ; j++){
-// //         // document.write("*");
-// //         if(j % 2 == 0){
-// //             document.write("A");
-// //         }else{
-// //             document.write("*");
-// //         }
-// //     }
-// //     document.write("<br>");
-// // }
+// js 1st task start here
+for(let i = 1 ; i<= 7 ; i+=2){
+    for( let k = i ; k<7 ; k++){
+        document.write("&nbsp;");
+    }
+    for(let j = 1 ; j<=i ; j++){
+        // document.write("*");
+        if(j % 2 == 0){
+            document.write("A");
+        }else{
+            document.write("*");
+        }
+    }
+    document.write("<br>");
+}
 // // js 1st task end here
 // // // js 6th task start here
-// // for(let i = 1; i <= 6 ; i++){
-// //     // document.write("*");
-// //     for(let j = 1 ; j<= i ; j++){
-// //          document.write("#");
-// //     }
-// //     document.write("<br>");
-// // }
+for(let i = 1; i <= 6 ; i++){
+    // document.write("*");
+    for(let j = 1 ; j<= i ; j++){
+         document.write("#");
+    }
+    document.write("<br>");
+}
 // // // js 6th task end here
 // // JS 2nd task start here
-// // for(let row = 1 ; row<= 5 ;row++){
-// //     for( let col = 1 ; col <= 9 ; col++){
-// //         if( (row ==5) || (row+col==6) || (col-row==4)){
-// //             document.write("*");
-// //         }else{
-// //             document.write("&nbsp;&nbsp");
+for(let row = 1 ; row<= 5 ;row++){
+    for( let col = 1 ; col <= 9 ; col++){
+        if( (row ==5) || (row+col==6) || (col-row==4)){
+            document.write("*");
+        }else{
+            document.write("&nbsp;&nbsp");
 
-// //         }
-// //     }
-// //     document.write("<br>");
-// // }
+        }
+    }
+    document.write("<br>");
+}
 // // // JS 2nd task end here
 // // JS 3rd task start here
-// // for ( let i = 1 ; i<= 3 ; i++){
-// //     for( let j = 1 ; j<= i ; j++){
-// //         document.write(j**i + " ");
-// //     }
-// //     document.write("<br>");
-// // }
+for ( let i = 1 ; i<= 3 ; i++){
+    for( let j = 1 ; j<= i ; j++){
+        document.write(j**i + " ");
+    }
+    document.write("<br>");
+}
 // // JS 3rd task end here
 // // JS 5th Task start here
-// for( let i = 1 ; i<=10 ; i++){
-//     for( let j = 1 ; j<=10 ; j++){
-//         if((i==5 || i==6) || (i==1 && (j==1 || j==10)) || (i==2 && (j<3 || j>8)) || (i==3 && (j<4 || j>7)) || (i==4 && (j<5 || j>6)) || (i==7 && (j<5 || j>6)) || (i==8 && (j<4 || j>7)) || (i==9 && (j<3 || j>8)) || (i==10 && (j<2 || j>9))) {
-//             document.write("*");
-//         } else{
-//         document.write("&nbsp;&nbsp;");
-//         }
-//     }
-//     document.write("<br>");
-// }
+for( let i = 1 ; i<=10 ; i++){
+    for( let j = 1 ; j<=10 ; j++){
+        if((i==5 || i==6) || (i==1 && (j==1 || j==10)) || (i==2 && (j<3 || j>8)) || (i==3 && (j<4 || j>7)) || (i==4 && (j<5 || j>6)) || (i==7 && (j<5 || j>6)) || (i==8 && (j<4 || j>7)) || (i==9 && (j<3 || j>8)) || (i==10 && (j<2 || j>9))) {
+            document.write("*");
+        } else{
+        document.write("&nbsp;&nbsp;");
+        }
+    }
+    document.write("<br>");
+}
 //JS 5th Task end here
 //JS 4th Task start here
-// for (let i = 1; i <= 9; i++) {
-// for( let j = 1 ; j<= 9 ; j++){
-// if (( (i==1 || i==9)) || (i==2 && (j<5 || j>5)) || (i==3 && (j<4 || j>6)) || (i==4 && (j<3 || j>7)) || (i==5 && (j<2 || j>8)) || (i==6 && (j>7 || j<3)) || (i==7 && (j>6 || j<4)) || (i==8 && (j>5 || j<5)) ){
-//     document.write("*")
-// }else{
-//     document.write("&nbsp;&nbsp;");
-//     }
-// }
-// document.write("<br>");
-// }
+for (let i = 1; i <= 9; i++) {
+for( let j = 1 ; j<= 9 ; j++){
+if (( (i==1 || i==9)) || (i==2 && (j<5 || j>5)) || (i==3 && (j<4 || j>6)) || (i==4 && (j<3 || j>7)) || (i==5 && (j<2 || j>8)) || (i==6 && (j>7 || j<3)) || (i==7 && (j>6 || j<4)) || (i==8 && (j>5 || j<5)) ){
+    document.write("*")
+}else{
+    document.write("&nbsp;&nbsp;");
+    }
+}
+document.write("<br>");
+}
 // JS 4th Task end here
 
 
