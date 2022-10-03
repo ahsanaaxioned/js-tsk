68517d2ae51ddc9c51400393f608e4d628355fd9
ahsanaaxioned <ahsana@axioned.com>
2022-10-03 13:03:03 +0530

    JS 4th task completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@
 // //             document.write("*");
 // //         }else{
 // //             document.write("&nbsp;&nbsp");
-           
+
 // //         }
 // //     }
 // //     document.write("<br>");
@@ -44,19 +44,32 @@
 // //     document.write("<br>");
 // // }
 // // JS 3rd task end here
-// JS 5th Task start here
-for( let i = 1 ; i<=10 ; i++){
-    for( let j = 1 ; j<=10 ; j++){
-        if((i==5 || i==6) || (i==1 && (j==1 || j==10)) || (i==2 && (j<3 || j>8)) || (i==3 && (j<4 || j>7)) || (i==4 && (j<5 || j>6)) || (i==7 && (j<5 || j>6)) || (i==8 && (j<4 || j>7)) || (i==9 && (j<3 || j>8)) || (i==10 && (j<2 || j>9))) {
-            document.write("*");
-        } else{
-        document.write("&nbsp;&nbsp;");
-        }
-    }
-    document.write("<br>");
-}
-// JS 5th Task end here
-    
+// // JS 5th Task start here
+// for( let i = 1 ; i<=10 ; i++){
+//     for( let j = 1 ; j<=10 ; j++){
+//         if((i==5 || i==6) || (i==1 && (j==1 || j==10)) || (i==2 && (j<3 || j>8)) || (i==3 && (j<4 || j>7)) || (i==4 && (j<5 || j>6)) || (i==7 && (j<5 || j>6)) || (i==8 && (j<4 || j>7)) || (i==9 && (j<3 || j>8)) || (i==10 && (j<2 || j>9))) {
+//             document.write("*");
+//         } else{
+//         document.write("&nbsp;&nbsp;");
+//         }
+//     }
+//     document.write("<br>");
+// }
+//JS 5th Task end here
+//JS 4th Task start here
+// for (let i = 1; i <= 9; i++) {
+// for( let j = 1 ; j<= 9 ; j++){
+// if (( (i==1 || i==9)) || (i==2 && (j<5 || j>5)) || (i==3 && (j<4 || j>6)) || (i==4 && (j<3 || j>7)) || (i==5 && (j<2 || j>8)) || (i==6 && (j>7 || j<3)) || (i==7 && (j>6 || j<4)) || (i==8 && (j>5 || j<5)) ){
+//     document.write("*")
+// }else{
+//     document.write("&nbsp;&nbsp;");
+//     }
+// }
+// document.write("<br>");
+// }
+// JS 4th Task end here
+
+
 
 
 
